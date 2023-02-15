ccd0c04f9bde57dacb6e29c4f91719b55d606f06
hivi6 <tthreehivi@gmail.com>
2023-02-15 18:45:39 +0530

    Added some more functionality to MyArrayList

---
+++ generics/GenericArrayDemo.java
@@ -0,0 +1,44 @@
+package generics;
+
+class GenericArray<T> {
+    private T[] elems;
+    GenericArray(T[] e) {
+        elems = e;
+    }
+    GenericArray(int cap) {
+        elems = (T[]) new Object[cap];
+    }
+
+    public void set(int index, T element) {
+        if (elems == null || index < 0 || index >= elems.length)
+            throw new IndexOutOfBoundsException();
+        elems[index] = element;
+    }
+
+    public T get(int index) {
+        if (elems == null || index < 0 || index >= elems.length)
+            throw new IndexOutOfBoundsException();
+        return elems[index];
+    }
+
+    public String toString() {
+        if (elems == null)
+            return "[]";
+        String res = "";
+        for (int i = 0; i < elems.length; i++) {
+            if (i != 0)
+                res += " ";
+            res += elems[i];
+        }
+        return "[" + res + "]";
+    }
+}
+
+public class GenericArrayDemo {
+    public static void main(String[] args) {
+        GenericArray<String> a = new GenericArray<>(10);
+        a.set(1, "element-1");
+        a.set(3, "no-null-value-lol");
+        System.out.println(a); 
+    }
+}
--- my_datastructures/MyArrayListDemo.java
+++ my_datastructures/MyArrayListDemo.java
@@ -102,7 +102,7 @@ class MyArrayList {
     }
 
     public Integer set(int index, Integer element) {
-        if (index < 0 || index >= size()) 
+        if (index < 0 || index >= size())
             throw new IndexOutOfBoundsException();
         elems[index] = element;
         return element;
@@ -116,7 +116,7 @@ class MyArrayList {
         len++;
         return true;
     }
-    
+
     public void add(int index, Integer element) {
         if (index < 0 || index > size())
             throw new IndexOutOfBoundsException();
@@ -142,7 +142,8 @@ class MyArrayList {
 
     public boolean remove(Object obj) {
         var index = indexOf(obj);
-        if (index == -1) return false;
+        if (index == -1)
+            return false;
         remove(index);
         return true;
     }
@@ -160,10 +161,91 @@ class MyArrayList {
             add(list.elems[i]);
         return !list.isEmpty();
     }
+
+    public boolean addAll(int index, MyArrayList list) throws IndexOutOfBoundsException, NullPointerException {
+        if (index < 0 || index > size())
+            throw new IndexOutOfBoundsException();
+        if (list == null)
+            throw new NullPointerException();
+        ensureCapacity(len + list.len);
+        int i = len + list.len - 1;
+        while (i >= index + list.len) {
+            elems[i] = elems[i - list.len];
+            i--;
+        }
+        for (int j = index; j < index + list.len; j++)
+            elems[j] = list.elems[j - index];
+        len += list.len;
+        return !list.isEmpty();
+    }
+
+    protected void removeRange(int fromIndex, int toIndex) throws IndexOutOfBoundsException {
+        if (fromIndex < 0 || fromIndex >= size() || toIndex > size() || toIndex < fromIndex)
+            throw new IndexOutOfBoundsException();
+        // [1, 2, 3, 4, 5, 6]
+        // (1, 3) remove
+        // [1, 4, 5, 6]
+        for (int i = toIndex; i < len; i++)
+            elems[i - (toIndex - fromIndex)] = elems[i];
+        len -= (toIndex - fromIndex);
+    }
+
+    private void mergeSort(int start, int end) {
+        // both start and end inclusive
+        if (start >= end)
+            return;
+        if (start < end) {
+            var mid = start + (end - start) / 2;
+            mergeSort(start, mid);
+            mergeSort(mid + 1, end);
+            // merge
+            Integer[] left = new Integer[mid - start + 1];
+            Integer[] right = new Integer[end - mid];
+            for (int i = start; i <= mid; i++)
+                left[i - start] = elems[i];
+            for (int i = mid + 1; i <= end; i++)
+                right[i - mid - 1] = elems[i];
+            int i = 0, j = 0, k = start;
+            while (i < (mid - start + 1) && j < (end - mid)) {
+                if (left[i] <= right[j])
+                    elems[k++] = left[i++];
+                else
+                    elems[k++] = right[j++];
+            }
+            while (i < (mid - start + 1))
+                elems[k++] = left[i++];
+            while (j < (end - mid))
+                elems[k++] = right[j++];
+        }
+    }
+
+    public void sort() {
+        mergeSort(0, size() - 1);
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < len; i++) {
+            if (i != 0)
+                res += " ";
+            res += elems[i];
+        }
+        return "[" + res + "]";
+    }
 }
 
 public class MyArrayListDemo {
     public static void main(String[] args) {
         MyArrayList a = new MyArrayList();
+        a.add(5);
+        a.add(4);
+        a.add(3);
+        a.add(2);
+        a.add(1);
+        System.out.println(a);
+        a.sort();
+        System.out.println(a);
+        a.removeRange(0, a.size());
+        System.out.println(a);
     }
 }
